1e492a2357fb2450fd6ac7a2de0ff22b43f79fa7
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 18:40:22 +0100

    improvements and fixed spelling

--- source/bb/util/file/database/FileWriter.java
+++ source/bb/util/file/database/FileWriter.java
@@ -1,5 +1,7 @@
 package bb.util.file.database;
 
+import bb.util.exceptions.WrongVersionException;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -7,48 +9,43 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import bb.util.exceptions.WrongVersionException;
-
 /**
  * @author BB20101997
  */
 @SuppressWarnings("javadoc")
-public final class FileWriter
-{
+public final class FileWriter {
 
 	/**
-	 * The Vesion of the FileWriter
+	 * The Version of the FileWriter
 	 */
-	private final float			VERSION					= 0.01f;
+	private final float VERSION = 0.01f;
 
-	// kann bis mindestens F8FF fortgeführt werden
+	// kann bis mindestens F8FF fortgefÃ¼hrt werden
 
-	public static final char	splitCharacter			= '\uF8F0';
-	public static final char	typeSeperator			= '\uF8F5';
+	public static final char splitCharacter = '\uF8F0';
+	public static final char typeSeparator  = '\uF8F5';
 
-	public static final char	innerFileWriterBeginn	= '\uF8F7';
-	public static final char	innerFileWriterEnd		= '\uF8F8';
+	public static final char innerFileWriterBeginn = '\uF8F7';
+	public static final char innerFileWriterEnd    = '\uF8F8';
 
-	public static final char	beginOfString			= '\uF8F1';
-	public static final char	endOfString				= '\uF8F2';
+	public static final char beginOfString = '\uF8F1';
+	public static final char endOfString   = '\uF8F2';
 
-	public static final char	escapeCaracter			= '\uF8F3';
+	public static final char escapeCharacter = '\uF8F3';
 
-	public static final char	endOfFileWriter			= '\uF8F4';
+	public static final char endOfFileWriter = '\uF8F4';
 
-	public static final String	VersionName				= "\uF8F6" + "VERSION";
+	public static final String VersionName = "\uF8F6" + "VERSION";
 
 	/**
 	 * @author BB20101997
 	 */
-	private enum Types
-	{
+	private enum Types {
 		INT, STRING, ISAVEABLE, BYTE, BOOLEAN, FLOAT, DOUBLE, SHORT, LONG, CHAR
 
 	}
 
-	public FileWriter()
-	{
+	public FileWriter() {
 
 		add(VERSION, VersionName);
 	}
@@ -56,35 +53,32 @@ public final class FileWriter
 	/**
 	 * A list of Objects , to be saved or to be read
 	 */
-	private List<Object>	ObjectList	= new ArrayList<Object>();
+	private List<Object> ObjectList  = new ArrayList<>();
 	/**
 	 * The names of the Objects for easy retrieval
 	 */
-	private List<String>	ObjectNames	= new ArrayList<String>();
+	private List<String> ObjectNames = new ArrayList<>();
 	/**
 	 * The Type of Object saved in the ObjectList
 	 */
-	private List<Types>		ObjectType	= new ArrayList<Types>();
+	private List<Types>  ObjectType  = new ArrayList<>();
 
-	private Object toObject(String name, String type, String s)
-	{
+	private Object toObject(String name, String type, String s) {
 
 		return null;
 	}
 
-	private Object toObject(String name, String type, FileWriter fw)
-	{
+	private Object toObject(String name, String type, FileWriter fw) {
 
 		return null;
 	}
 
 	/**
-	 * @param is
-	 *            the Inputstream to read the Objects from
+	 * @param is the InputStream to read the Objects from
+	 *
 	 * @return if reading was successful
 	 */
-	public boolean readFromStream(InputStream is) throws WrongVersionException
-	{
+	public boolean readFromStream(InputStream is) throws WrongVersionException {
 
 		InputStreamReader ISR = new InputStreamReader(is);
 
@@ -94,103 +88,97 @@ public final class FileWriter
 		int i;
 
 		// 0 if at the Name,1 if Type,2 if value
-		int postition = 0;
+		int position = 0;
 		String name = "";
 		String type = "";
 		String s = "";
 		boolean inString = false;
 		FileWriter fw = null;
-		try
-		{
-			wloop:
-			while((i = ISR.read()) != -1)
-			{
+		try {
+			//wloop:
+			while((i = ISR.read()) != -1) {
 				c = (char) i;
-				if(!inString)
-				{
+				if(!inString) {
 
-					if((c == endOfFileWriter))
-					{
-						break wloop;
+					if(c == endOfFileWriter) {
+						//break wloop;
 					}
 
-					if(c == splitCharacter)
-					{
-						if(type.equals(String.valueOf(Types.ISAVEABLE.ordinal())))
-						{
+					if(c == splitCharacter) {
+						if(type.equals(String.valueOf(Types.ISAVEABLE.ordinal()))) {
 							toObject(name, type, fw);
-						}
-						else
-						{
+						} else {
 							toObject(name, type, s);
 						}
-						postition = 0;
+						position = 0;
 					}
 
-					if(c == innerFileWriterBeginn)
-					{
+					if(c == innerFileWriterBeginn) {
 						fw = new FileWriter();
 						fw.readFromStream(is);
 					}
 
-					if(c == typeSeperator)
-					{
+					if(c == typeSeparator) {
 
-						switch(postition){
-							case 0 : {
+						switch(position) {
+							case 0: {
 								name = s;
 								break;
 							}
-							case 1 : {
+							case 1: {
 								type = s;
 								break;
 							}
 						}
 						s = "";
-						postition++;
+						position++;
 					}
 
 				}
 
-				if((c == endOfString) && !lastEscape)
-				{
+				if((c == endOfString) && !lastEscape) {
 					inString = false;
-					lastEscape = false;
 				}
 
-				if(inString && ((c != escapeCaracter) && !lastEscape))
-				{
-					s = s + c;
+				if(inString) {
+					if(c != escapeCharacter){
+						if(!lastEscape) {
+							s = s + c;
+						}
+						else{
+							//TODO:should never appear?!
+						}
+					}
+					else if(lastEscape){
+						s = s+ c;
+					}
+					else{
+						lastEscape = true;
+					}
 				}
 
-				if(c == beginOfString)
-				{
+				if(c == beginOfString) {
 					inString = true;
 				}
 			}
-		}
-		catch(IOException e)
-		{
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
 	/**
-	 * @param os
-	 *            the Outputstream to be written to
+	 * @param os the OutputStream to be written to
+	 *
 	 * @return if the execution was successful
-	 * @throws IOException
-	 *             is thrown when the writing of one Objet to Steam fails
+	 *
+	 * @throws IOException is thrown when the writing of one Object to Steam fails
 	 */
-	public boolean writeToStream(OutputStream os) throws IOException
-	{
+	public boolean writeToStream(OutputStream os) throws IOException {
 
 		boolean noFail = true;
-		for(int i = 0; i < ObjectList.size(); i++)
-		{
-			if(!writeObjectToStream(ObjectList.get(i), ObjectType.get(i), ObjectNames.get(i), os))
-			{
+		for(int i = 0; i < ObjectList.size(); i++) {
+			if(!writeObjectToStream(ObjectList.get(i), ObjectType.get(i), ObjectNames.get(i), os)) {
 				noFail = false;
 				throw new IOException();
 			}
@@ -200,27 +188,22 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param obj
-	 *            the object to be written
-	 * @param t
-	 *            the type of the Object
-	 * @param name
-	 *            the name of the Object
-	 * @param os
-	 *            the Outputstream to be written to
-	 * @return if the Outprinting was successful
+	 * @param obj  the object to be written
+	 * @param t    the type of the Object
+	 * @param name the name of the Object
+	 * @param os   the OutputStream to be written to
+	 *
+	 * @return if the operation was successful
 	 */
-	private boolean writeObjectToStream(Object obj, Types t, String name, OutputStream os)
-	{
+	private boolean writeObjectToStream(Object obj, Types t, String name, OutputStream os) {
 
 		boolean noFail = true;
 
-		try
-		{
-			os.write((name + typeSeperator + t.ordinal() + typeSeperator).getBytes());
+		try {
+			os.write((name + typeSeparator + t.ordinal() + typeSeparator).getBytes());
 
-			switch(t){
-				case ISAVEABLE : {
+			switch(t) {
+				case ISAVEABLE: {
 					os.write(beginOfString);
 					ISaveAble is = (ISaveAble) obj;
 					FileWriter fw = new FileWriter();
@@ -229,14 +212,12 @@ public final class FileWriter
 					os.write(endOfString);
 					break;
 				}
-				default : {
+				default: {
 					os.write((beginOfString + String.valueOf(obj) + endOfString).getBytes());
 					break;
 				}
 			}
-		}
-		catch(Exception e)
-		{
+		} catch(Exception e) {
 			noFail = false;
 			e.printStackTrace();
 		}
@@ -245,13 +226,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the Integer to be added
-	 * @param name
-	 *            the Name of the Integer
+	 * @param i    the Integer to be added
+	 * @param name the Name of the Integer
 	 */
-	public void add(int i, String name)
-	{
+	public void add(int i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -259,13 +237,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the String to be added
-	 * @param name
-	 *            the name of the String
+	 * @param i    the String to be added
+	 * @param name the name of the String
 	 */
-	public void add(String i, String name)
-	{
+	public void add(String i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -273,13 +248,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the ISaveAble to be added
-	 * @param name
-	 *            the name of the ISaveAble
+	 * @param i    the ISaveAble to be added
+	 * @param name the name of the ISaveAble
 	 */
-	public void add(ISaveAble i, String name)
-	{
+	public void add(ISaveAble i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -287,13 +259,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the byte to be added
-	 * @param name
-	 *            the name of the byte
+	 * @param i    the byte to be added
+	 * @param name the name of the byte
 	 */
-	public void add(byte i, String name)
-	{
+	public void add(byte i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -301,13 +270,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the boolean to be added
-	 * @param name
-	 *            the name of the boolean
+	 * @param i    the boolean to be added
+	 * @param name the name of the boolean
 	 */
-	public void add(boolean i, String name)
-	{
+	public void add(boolean i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -315,13 +281,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the float to be added
-	 * @param name
-	 *            the name of the float
+	 * @param i    the float to be added
+	 * @param name the name of the float
 	 */
-	public void add(float i, String name)
-	{
+	public void add(float i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -329,13 +292,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the double to be added
-	 * @param name
-	 *            the name of the double
+	 * @param i    the double to be added
+	 * @param name the name of the double
 	 */
-	public void add(double i, String name)
-	{
+	public void add(double i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -343,13 +303,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the short to be added
-	 * @param name
-	 *            the name of the short
+	 * @param i    the short to be added
+	 * @param name the name of the short
 	 */
-	public void add(short i, String name)
-	{
+	public void add(short i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -357,13 +314,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the long to be added
-	 * @param name
-	 *            the name of the long
+	 * @param i    the long to be added
+	 * @param name the name of the long
 	 */
-	public void add(long i, String name)
-	{
+	public void add(long i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
@@ -371,13 +325,10 @@ public final class FileWriter
 	}
 
 	/**
-	 * @param i
-	 *            the chat to be added
-	 * @param name
-	 *            the name of the char
+	 * @param i    the chat to be added
+	 * @param name the name of the char
 	 */
-	public void add(char i, String name)
-	{
+	public void add(char i, String name) {
 
 		ObjectList.add(i);
 		ObjectNames.add(name);
