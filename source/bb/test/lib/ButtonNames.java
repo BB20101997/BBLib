4ac8f015baf254da128c66f20af0a18d5acb1b6d
BB20101997 <blessmann-bennet@t-online.de>
2014-10-29 18:40:58 +0100

    added the name for the ChangeObject

--- source/bb/test/lib/ButtonNames.java
+++ source/bb/test/lib/ButtonNames.java
@@ -4,5 +4,6 @@ public class ButtonNames
 {
 	public static final String	NEWOBJBUTTON	= "New Object";
 	public static final String	ENTER			= "Enter";
+	public static final String	CHANGEOBJ		= "Change Object";
 
 }
