1c6146fb972c8ce7c72341084a1f06463d7b796f
BB20101997 <blessmann-bennet@t-online.de>
2014-11-24 20:23:08 +0100

    added a Safty Comment

--- src/bb/util/file/database/ISaveAble.java
+++ src/bb/util/file/database/ISaveAble.java
@@ -1,7 +1,7 @@
 package bb.util.file.database;
 
 public interface ISaveAble {
-	
+	//Beware don´t save an ISaveAble inside itself, will cause endless loop!!!
 	public abstract void writeToFileWriter(FileWriter fw);
 	
 	public abstract void loadFromFileWriter(FileWriter fw);
