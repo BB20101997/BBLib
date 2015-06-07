448a12f64106e874c1bcb60e51df5a8e41949345
BB20101997 <blessmann-bennet@t-online.de>
2015-06-07 22:09:41 +0200

    added a Test for the LogHandler

--- test/bb/util/file/LogHandlerTest.java
+++ test/bb/util/file/LogHandlerTest.java
@@ -1,28 +1,36 @@
 package bb.util.file;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
 
 /**
  * Created by BB20101997 on 06.06.2015.
  */
 public class LogHandlerTest {
 
+	Logger l;
+
 	@Before
 	public void setUp() throws Exception {
-
-	}
-
-	@After
-	public void tearDown() throws Exception {
-
+		File f = new File("/log/"+new SimpleDateFormat("dd-MM-yyyy").format(new Date())+"-log.txt");
+		l = Logger.getLogger("bb.util.file.LogHandlerTest");
+		l.addHandler(new LogHandler("LogHandlerTest",f.getCanonicalFile()));
 	}
 
 	@Test
 	public void testPublish() throws Exception {
-
+		l.info("Test Message");
+		StringBuilder sb = new StringBuilder();
+		for(int i= 1;i<=10;i++){
+			sb.append("Test Line ").append(i).append("!");
+			if(i!=100){
+			sb.append("\n");
+		}}
+		l.info(sb.toString());
 	}
 }
