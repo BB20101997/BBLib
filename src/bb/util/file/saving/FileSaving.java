dcf97216ae6f833ead92cc14486e16533f4fa3eb
BB20101997 <blessmann-bennet@t-online.de>
2016-04-12 21:10:10 +0200

    FileSaving and SaveInstance changed

--- src/bb/util/file/saving/FileSaving.java
+++ src/bb/util/file/saving/FileSaving.java
@@ -1,67 +1,54 @@
 package bb.util.file.saving;
 
 import bb.util.file.FileChooser;
-import com.sun.istack.internal.NotNull;
 
 import javax.swing.*;
 import java.io.File;
 import java.nio.file.attribute.FileTime;
-import java.util.ResourceBundle;
 
 /**
  * Created by BB20101997 on 28.07.2015.
  */
 public class FileSaving {
 
-	File      open;
-	FileTime  lastSaving;
-	JMenuItem newFile, save, saveAs, load;
-	final Instanter          instanter;
-	final Instanter.Instance instance;
+	private File      open;
+	private FileTime  lastSaving;
+	private JMenuItem newFile, save, saveAs, load;
+	private SaveInstance instance;
 
-	public interface Instanter {
-
-		//create a new Instance or some how return an Instance
-		Instance createInstance();
-
-		interface Instance {
-
-			//load up the content of the given file
-			boolean loadFromFile(File f);
-
-			//save to the given File
-			boolean saveToFile(File f);
-
-			//have changes been made since the last resetChanged
-			boolean hasChanged();
-
-			//reset that changes have been mad, do't reset the changes
-			void resetChanged();
-
-			//reset,new creation of a dokument save what ever
-			boolean resetData();
-		}
-	}
-
-
-	public FileSaving(@NotNull Instanter i) {
-		this(i, i.createInstance());
-	}
-
-	public FileSaving(Instanter i, Instanter.Instance ins) {
-		instanter = i;
+	public FileSaving(SaveInstance ins) {
 		instance = ins;
+		initMenuItems();
+	}
+
+	private void initMenuItems(){
+		//init New
+		newFile = new JMenuItem("New");
+		newFile.addActionListener(e -> executeNew());
+
+		//init Save
+		save = new JMenuItem("Save");
+		save.addActionListener(e -> executeSave());
+
+		//init SaveAs
+		saveAs = new JMenuItem("SaveAs");
+		saveAs.addActionListener(e -> executeSaveAs());
 	}
 
 	//returns the MenuItem for performing a
-	public JMenuItem getMenuItemNew() {
-		if(newFile == null) {
-			newFile = new JMenuItem("New");
-			newFile.addActionListener(e -> executeNew());
-		}
+	public JMenuItem getNew() {
 		return newFile;
 	}
 
+	public JMenuItem getSave(){
+		return save;
+	}
+
+	public JMenuItem getSaveAs() {
+		return saveAs;
+	}
+
+	@SuppressWarnings("WeakerAccess")
 	public void executeNew() {
 		if(instance.hasChanged()) {
 			int i = JOptionPane.showInternalConfirmDialog(newFile, "Do you want to save your changes before starting a New one?", "Do you consider saving?", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
@@ -85,39 +72,32 @@ public class FileSaving {
 		}
 	}
 
+	@SuppressWarnings("WeakerAccess")
 	public void executeSave() {
 		if(instance.hasChanged()) {
-			if(open != null) {
+			if(open == null) {
 				open = FileChooser.chooseFile(null, "Save", JFileChooser.FILES_ONLY);
 				if(open == null) {
 					JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:3)\nAborting saving!");
 					return;
 				}
 			}
-			if(instance.saveToFile(open)) {
+			if(!instance.saveToFile(open)) {
 				JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:4)\nAborting Creating a New File!\nIf you tried overriding your last save it might be corrupted!");
-				return;
 			}
 		}
 	}
 
+	@SuppressWarnings("WeakerAccess")
 	public void executeSaveAs() {
-		if(instance.hasChanged()) {
-
-			open = FileChooser.chooseFile(null, "Save", JFileChooser.FILES_ONLY);
-			if(open == null) {
-				JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:3)\nAborting saving!");
-				return;
-			}
-
-			if(instance.saveToFile(open)) {
-				JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:4)\nAborting Creating a New File!\nIf you tried overriding your last save it might be corrupted!");
-				return;
-			}
+		open = FileChooser.chooseFile(null, "Save", JFileChooser.FILES_ONLY);
+		if(open == null) {
+			JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:3)\nAborting saving!");
+			return;
 		}
-	}
-	{
-		ResourceBundle.getBundle("test");
+		if(!instance.saveToFile(open)) {
+			JOptionPane.showConfirmDialog(newFile, "Something went wrong while saving!(ERR:4)\nAborting Creating a New File!\nIf you tried overriding your last save it might be corrupted!");
+		}
 	}
 
 }
--- src/bb/util/file/saving/SaveInstance.java
+++ src/bb/util/file/saving/SaveInstance.java
@@ -1,7 +1,25 @@
 package bb.util.file.saving;
 
+import java.io.File;
+
 /**
  * Created by BB20101997 on 08. Apr. 2016.
  */
 public interface SaveInstance {
+
+	//load up the content of the given file
+	boolean loadFromFile(File f);
+
+	//save to the given File
+	boolean saveToFile(File f);
+
+	//have changes been made since the last resetChanged
+	boolean hasChanged();
+
+	//reset that changes have been mad, do't reset the changes
+	void resetChanged();
+
+	//reset,new creation of a dokument save what ever
+	boolean resetData();
+
 }
