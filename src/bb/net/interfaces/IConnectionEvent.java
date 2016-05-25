0f1161bb4c3b5a406e4a833b932020f7b35bec8d
BB20101997 <blessmann-bennet@t-online.de>
2016-05-26 00:01:26 +0200

    + added the functional anotation

--- src/bb/net/interfaces/IConnectionEvent.java
+++ src/bb/net/interfaces/IConnectionEvent.java
@@ -5,6 +5,7 @@ import bb.util.event.IEvent;
 /**
  * Created by BB20101997 on 10.04.2015.
  */
+@FunctionalInterface
 public interface IConnectionEvent extends IEvent {
 
 	IIOHandler getIIOHandler();
