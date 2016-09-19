1253a151226e2d765644378ec7c6526d6bf4ae75
BB20101997 <blessmann-bennet@t-online.de>
2016-09-19 22:36:36 +0200

    ~ now using Streams

--- src/main/java/bb/net/util/Socket.java
+++ src/main/java/bb/net/util/Socket.java
@@ -2,8 +2,8 @@ package bb.net.util;
 
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLSocket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * Created by BB20101997 on 29.08.2014.
@@ -12,27 +12,17 @@ public class Socket {
 
 	public static void enableAnonConnection(SSLServerSocket socket) {
 		String[] a = socket.getSupportedCipherSuites();
-		List<String> b = new ArrayList<>();
-		for(String s : a) {
-			if(s.indexOf("_anon_") > 0) {
-				b.add(s);
-			}
-		}
-
-		String[] c = b.toArray(new String[b.size()]);
+		Stream<String> stream = Arrays.stream(a);
+		stream = stream.filter(s -> s.indexOf("_anon_")>0);
+		String[] c = stream.toArray(String[]::new);
 		socket.setEnabledCipherSuites(c);
 	}
 
 	public static void enableAnonConnection(SSLSocket socket) {
-		String[] a = socket.getSupportedCipherSuites();
-		List<String> b = new ArrayList<>();
-		for(String s : a) {
-			if(s.indexOf("_anon_") > 0) {
-				b.add(s);
-			}
-		}
-
-		String[] c = b.toArray(new String[b.size()]);
+		String[]       a      = socket.getSupportedCipherSuites();
+		Stream<String> stream = Arrays.stream(a);
+		stream = stream.filter(s -> s.indexOf("_anon_") > 0);
+		String[] c = stream.toArray(String[]::new);
 		socket.setEnabledCipherSuites(c);
 	}
 
