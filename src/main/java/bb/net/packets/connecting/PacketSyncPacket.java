137fe00ce5dc49e80a04f701bf635bd5b97c835a
BB20101997 <blessmann-bennet@t-online.de>
2016-09-19 22:37:19 +0200

    ~ Moving from Array to List implementation

--- src/main/java/bb/net/packets/connecting/PacketSyncPacket.java
+++ src/main/java/bb/net/packets/connecting/PacketSyncPacket.java
@@ -5,44 +5,61 @@ import bb.net.packets.DataIn;
 import bb.net.packets.DataOut;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by BB20101997 on 30.08.2014.
  */
 public class PacketSyncPacket extends APacket {
 
-	private Class<? extends APacket>[] classes;
+	private List<Class<? extends APacket>> classes;
 
+	@Deprecated
 	public PacketSyncPacket(Class<? extends APacket>[] cs) {
-		classes = cs;
+		classes = new ArrayList<>();
+		Collections.addAll(classes, cs);
 	}
 
+	public PacketSyncPacket(List<Class<? extends APacket>> clazzList){
+		classes = clazzList;
+	}
+
+	@SuppressWarnings("unused")
 	public PacketSyncPacket() {
+		//necessary for automatic instantiation
 	}
 
+	@Deprecated
 	public Class<? extends APacket>[] getPackageClasses() {
-		return classes;
+		//noinspection unchecked
+		return (Class<? extends APacket>[])classes.toArray();
+	}
+
+	public List<Class<? extends APacket>> getPackageClassList(){
+		return new ArrayList<>(classes);
 	}
 
 
 	@Override
 	public void writeToData(DataOut dataOut) throws IOException {
-		dataOut.writeInt(classes.length);
+		dataOut.writeInt(classes.size());
 		for(Class c : classes) {
 			dataOut.writeUTF(c.getName());
 		}
 	}
 
 	@Override
-	@SuppressWarnings("unchecked")
 	public void readFromData(DataIn dataIn) throws IOException {
 		int i = dataIn.readInt();
 
-		classes = new Class[i];
+		classes = new ArrayList<>();
 		while(i > 0) {
 			try {
-				Class c = Class.forName(dataIn.readUTF());
-				classes[classes.length - i] = c;
+				@SuppressWarnings("unchecked")
+				Class<? extends APacket> clazz = (Class<? extends APacket>) Class.forName(dataIn.readUTF());
+				classes.add(clazz);
 			} catch(ClassNotFoundException e) {
 				e.printStackTrace();
 			}
